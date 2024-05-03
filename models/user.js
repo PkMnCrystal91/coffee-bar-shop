26654bf16fb6286ff4e350dc42f4b08501b50753
Herbert Gutierrez <dev.gutti1391@gmail.com>
2024-05-03 16:36:53 -0600

    User Schema created

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = Schema({
+  name: {
+    type: String,
+    require: [true, "Please enter a name"],
+  },
+  email: {
+    type: String,
+    require: [true, " Please enter an email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: [true, "Please enter a password"],
+  },
+  img: {
+    type: String,
+  },
+  rol: {
+    type: String,
+    require: true,
+    enum: ["ADMIN_ROLE", "USER_ROLE"],
+  },
+  status: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = model("User", UserSchema);
